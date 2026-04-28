3f0681ccec38204e0b7f27eba85c15e53fa8f6dc
Jaydevlee <yousick5@gmail.com>
2026-04-28 11:21:18 +0900

    feature: recipt

--- Cafe_Pos/Forms/Form_Recipt.Designer.cs
+++ Cafe_Pos/Forms/Form_Recipt.Designer.cs
@@ -29,7 +29,15 @@
         private void InitializeComponent()
         {
             lblStore = new Label();
-            label1 = new Label();
+            lblDateTime = new Label();
+            lstOrderList = new ListBox();
+            pnPrice = new Panel();
+            lblGreeting = new Label();
+            lblTotal = new Label();
+            lblReceived = new Label();
+            lblCharge = new Label();
+            btnClose = new Button();
+            pnPrice.SuspendLayout();
             SuspendLayout();
             // 
             // lblStore
@@ -43,30 +51,103 @@
             lblStore.Text = "커피한잔";
             lblStore.TextAlign = ContentAlignment.MiddleCenter;
             // 
-            // label1
+            // lblDateTime
             // 
-            label1.Location = new Point(212, 101);
-            label1.Name = "label1";
-            label1.Size = new Size(89, 25);
-            label1.TabIndex = 1;
-            label1.Text = "시간날짜";
+            lblDateTime.Location = new Point(212, 101);
+            lblDateTime.Name = "lblDateTime";
+            lblDateTime.Size = new Size(89, 25);
+            lblDateTime.TabIndex = 1;
+            lblDateTime.Text = "시간날짜";
+            // 
+            // lstOrderList
+            // 
+            lstOrderList.FormattingEnabled = true;
+            lstOrderList.Location = new Point(29, 129);
+            lstOrderList.Name = "lstOrderList";
+            lstOrderList.Size = new Size(469, 184);
+            lstOrderList.TabIndex = 2;
+            // 
+            // pnPrice
+            // 
+            pnPrice.Controls.Add(lblCharge);
+            pnPrice.Controls.Add(lblReceived);
+            pnPrice.Controls.Add(lblTotal);
+            pnPrice.Location = new Point(212, 329);
+            pnPrice.Name = "pnPrice";
+            pnPrice.Size = new Size(286, 162);
+            pnPrice.TabIndex = 3;
+            // 
+            // lblGreeting
+            // 
+            lblGreeting.Font = new Font("맑은 고딕", 14F);
+            lblGreeting.Location = new Point(122, 506);
+            lblGreeting.Name = "lblGreeting";
+            lblGreeting.Size = new Size(264, 38);
+            lblGreeting.TabIndex = 4;
+            lblGreeting.Text = "감사합니다. 빨리 나가";
+            // 
+            // lblTotal
+            // 
+            lblTotal.Location = new Point(20, 14);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(210, 25);
+            lblTotal.TabIndex = 2;
+            lblTotal.Text = "합계:";
+            // 
+            // lblReceived
+            // 
+            lblReceived.Location = new Point(20, 62);
+            lblReceived.Name = "lblReceived";
+            lblReceived.Size = new Size(179, 25);
+            lblReceived.TabIndex = 3;
+            lblReceived.Text = "받은 금액:";
+            // 
+            // lblCharge
+            // 
+            lblCharge.Location = new Point(20, 123);
+            lblCharge.Name = "lblCharge";
+            lblCharge.Size = new Size(196, 25);
+            lblCharge.TabIndex = 4;
+            lblCharge.Text = "거스름돈:";
+            // 
+            // btnClose
+            // 
+            btnClose.Dock = DockStyle.Bottom;
+            btnClose.Location = new Point(0, 639);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(530, 71);
+            btnClose.TabIndex = 5;
+            btnClose.Text = "닫기";
+            btnClose.UseVisualStyleBackColor = true;
             // 
             // Form_Recipt
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(530, 646);
-            Controls.Add(label1);
+            ClientSize = new Size(530, 710);
+            Controls.Add(btnClose);
+            Controls.Add(lblGreeting);
+            Controls.Add(pnPrice);
+            Controls.Add(lstOrderList);
+            Controls.Add(lblDateTime);
             Controls.Add(lblStore);
             Name = "Form_Recipt";
             StartPosition = FormStartPosition.CenterParent;
             Text = "영수증";
+            pnPrice.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Label lblStore;
-        private Label label1;
+        private Label lblDateTime;
+        private ListBox lstOrderList;
+        private Panel pnPrice;
+        private Label lblGreeting;
+        private Label lblCharge;
+        private Label lblReceived;
+        private Label lblTotal;
+        private Button btnClose;
     }
 }
--- Cafe_Pos/Forms/Form_Recipt.cs
+++ Cafe_Pos/Forms/Form_Recipt.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cafe_Pos.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,37 @@ namespace Cafe_Pos.Forms
 {
     public partial class Form_Recipt : Form
     {
-        public Form_Recipt()
+        long orderId;
+        string textTotal = "";
+        string textCharge = "";
+        string textRecevied = "";
+
+        private Dictionary<string, OrderItems> orderList = new Dictionary<string, OrderItems>();
+        private List<Orders> orders = new List<Orders>();
+
+        public Form_Recipt(long orderId, List<Orders> orders, Dictionary<string, OrderItems> orderList)
         {
             InitializeComponent();
+            this.orderId = orderId;
+            this.orders = orders;
+            this.orderList = orderList;
+        }
+
+
+        private void LoadOrderList()
+        {
+            foreach(OrderItems order in orderList.Values)
+            {
+                lstOrderList.Items.Add($"{order.Menu_name} {order.Quantity} {order.Subtotal}");
+            }
+        }
+
+        private void LoadPrice()
+        {
+            int total = orders[0].Total_amount;
+            int charge = orders[0].Change_amount;
+            int received = orders[0].Received_amount;
+            lblTotal.Text += $" {}"
         }
     }
 }
